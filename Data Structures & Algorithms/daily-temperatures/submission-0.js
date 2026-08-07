2ef0d8ada4856158400e99338c0636c06bf3e97a
Nirav2801 <133108299+Nirav2801@users.noreply.github.com>
2026-08-06 22:25:29 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) { 
+        const stack = []
+        const result = new Array(temperatures.length).fill(0)
+
+        for (let i = 0; i < temperatures.length; i++) {
+            while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                const topItemIndex = stack.pop();
+                result[topItemIndex] = i - topItemIndex;                
+            }
+            stack.push(i)
+        }
+        return result
+    }
+}
